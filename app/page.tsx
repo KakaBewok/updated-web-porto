747dcc4cd419a305eea20f9b5c53141615eea229
Noprizal <noprizal@voltras.co.id>
2025-12-18 09:24:28 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ export default function App() {
     <div className="min-h-screen bg-white">
       <Navbar />
       <Hero />
-      <Education />
       <Experience />
       <Portfolio />
+      <Education />
       <Contact />
       <Footer />
     </div>
